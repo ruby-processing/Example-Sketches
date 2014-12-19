684a8b03d5e0a9c70c3d853cc4b39cbc6f1dcd4a
monkstone <martin_p@lineone.net>
2014-12-19 07:34:17 +0000

    tidy up json example

--- samples/processing_app/topics/advanced_data/load_save_json.rb
+++ samples/processing_app/topics/advanced_data/load_save_json.rb
@@ -1,9 +1,9 @@
-# This example demonstrates how easily 'sketch data' can be retrieved from a json file
-# in ruby-processing. Note this sketch re-uses the Bubble class from the bubble library. 
-# The BubbleData class, can load, store and create instances of Bubble (and request them
-# to display and/or show their label, when 'mouse over').
+# This example demonstrates how 'sketch data' can be retrieved from a json file
+# in ruby-processing. Note this sketch re-uses the Bubble class from the bubble
+# library. The BubbleData class, can load, store and create instances of Bubble
+# (and request them to display and/or show their label, when 'mouse over').
 # @author Martin Prout, after Daniel Shiffmans version for processing
-# 
+require 'forwardable'
 require 'json'
 
 load_library :bubble
@@ -28,66 +28,64 @@ def mouse_pressed
   bubble_data.create_new_bubble(mouse_x, mouse_y)
 end
 
-class BubbleData 
+# This class can load store and create instances of Bubble
+class BubbleData
+  extend Forwardable
+  def_delegators(:@bubbles, :each, :<<, :size, :shift, :clear)
   include Enumerable
-  
+
   MAX_BUBBLE = 10
-  
+
   attr_reader :key, :path, :bubbles
-  
+
   # @param key String for top level hash
-  
-  def initialize key
+
+  def initialize(key)
     @key = key
     @bubbles = []
   end
-  
-  def each &block
-    bubbles.each &block
-  end  
-  
-  def create_new_bubble x, y
-    self.add Bubble.new(x, y, rand(40..80), 'new label')    
-    save_data 
+
+  def create_new_bubble(x, y)
+    add Bubble.new(x, y, rand(40..80), 'new label')
+    save_data
     load_data path
   end
-  
-  def display x, y
-    self.each do |bubble|
+
+  def display(x, y)
+    each do |bubble|
       bubble.display
       bubble.rollover(x, y)
     end
   end
-  
+
   # @param path to json file
-  
-  def load_data path
+
+  def load_data(path)
     @path = path
-    source_string = open(path, 'r'){ |file| file.read }
-    data = JSON.parse(source_string)[key]
-    bubbles.clear
+    file = File.read(path)
+    data = JSON.parse(file)[key]
+    clear
     # iterate the bubble_data array, and create an array of bubbles
     data.each do |point|
-      self.add Bubble.new(
+      add Bubble.new(
         point['position']['x'],
         point['position']['y'],
         point['diameter'],
         point['label'])
     end
   end
-  
-  def add bubble
-    bubbles << bubble
-    bubbles.shift if bubbles.size > MAX_BUBBLE
-  end 
-  
-  private   
-  
-  def save_data
-    hash = { key => self.map{ |point| point.to_hash } }
-    json = JSON.pretty_generate(hash)      # generate pretty output
-    open(path, 'w') { |f| f.write(json) }
-  end
-  
-end
 
+  def add(bubble)
+    bubbles << bubble
+    shift if size > MAX_BUBBLE
+  end
+
+  private
+
+  def save_data
+    hash = { key => map(&:to_hash) }
+    File.open(path, 'w') do |json|
+      json.write(JSON.pretty_generate(hash)) # generate pretty output
+    end
+  end
+end
