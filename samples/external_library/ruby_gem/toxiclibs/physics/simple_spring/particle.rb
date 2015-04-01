98c6e7529a8332c8b7018f6b5bef6c634586d683
monkstone <martin_p@lineone.net>
2015-04-01 11:37:12 +0100

    more samples

---
+++ samples/external_library/ruby_gem/toxiclibs/physics/simple_spring/particle.rb
@@ -0,0 +1,20 @@
+# The Nature of Code
+# Daniel Shiffman
+# http://natureofcode.com
+# Notice how we are using inheritance here!
+# We could have just stored a reference to a VerletParticle2D object
+# inside the Particle class, but inheritance is an alternative
+class Particle < Physics::VerletParticle2D
+  include Processing::Proxy
+  def initialize(loc)
+    super(loc)
+  end
+
+  # All we're doing really is adding a display function to a VerletParticle
+  def display
+    fill(127)
+    stroke(0)
+    stroke_weight(2)
+    ellipse(x, y, 32, 32)
+  end
+end
