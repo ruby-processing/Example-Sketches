f316f9e9ef587e8009cb6f0e15d8d4687ee18a24
monkstone <martin_p@lineone.net>
2015-04-13 21:14:50 +0100

    various

--- samples/external_library/java_processing/geomerative/hola_mundo.rb
+++ samples/external_library/java_processing/geomerative/hola_mundo.rb
@@ -11,27 +11,22 @@ attr_reader :grp
 def setup
   # Initialize the sketch
   size 600, 400
-  
   # VERY IMPORTANT: Always initialize the library in the setup
-  RG::init(self)
-
+  RG.init(self)
   # Set fill stroke colours
   fill(255, 102, 0)
   stroke(0)
-  
-  #  Load the font file we want to use (the file must be in the data folder in the sketch folder),
-  #  with the size 72 and the alignment CENTER
-  @grp = RG::get_text("Hola Mundo!", "FreeSans.ttf", 72, CENTER)
+  #  Load the font file we want to use (the file must be in the data folder in
+  # the sketch folder), with the size 72 and the alignment CENTER
+  @grp = RG.get_text('Hola Mundo!', 'FreeSans.ttf', 72, CENTER)
 end
 
 def draw
   # Clear frame
   background(255)
-  
   # Set the origin to draw in the middle of the sketch
   translate width / 2, height / 2
-  
-  # Draw the string "Hola Mundo!" on the PGraphics canvas g 
-  # (which is the default canvas of the sketch)  
+  # Draw the string 'Hola Mundo!' on the PGraphics canvas g
+  # (which is the default canvas of the sketch)
   grp.draw
 end
