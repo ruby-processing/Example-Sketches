caa3b908e8d31df2b4cd4474e0ea4222cc42b7a0
monkstone <martin_p@lineone.net>
2015-04-18 19:41:15 +0100

    modified:   samples/processing_app/basics/data/variable_scope.rb 	modified:   samples/processing_app/basics/data/variables.rb 	modified:   samples/processing_app/basics/math/additive_wave.rb 	modified:   samples/processing_app/basics/math/graphing_2_d_equation.rb 	modified:   samples/processing_app/basics/math/interpolate.rb 	modified:   samples/processing_app/basics/math/modulo.rb 	modified:   samples/processing_app/basics/math/random_gaussian.rb 	modified:   samples/processing_app/basics/math/sine.rb 	modified:   samples/processing_app/basics/math/sine_cosine.rb

--- samples/processing_app/basics/math/graphing_2_d_equation.rb
+++ samples/processing_app/basics/math/graphing_2_d_equation.rb
@@ -1,12 +1,12 @@
 #
 # Graphing 2D Equations
-# by Daniel Shiffman. 
-# 
-# Graphics the following equation: 
-# sin(n*cos(r) + 5*theta) 
-# where n is a function of horizontal mouse location.  
+# by Daniel Shiffman.
 #
- 
+# Graphics the following equation:
+# sin(n*cos(r) + 5*theta)
+# where n is a function of horizontal mouse location.
+#
+
 def setup
   size(640, 360)
 end
@@ -22,18 +22,17 @@ def draw
   width.times do |i|
     y = -h / 2        # Start y at -1 * height / 2
     height.times do |j|
-      r = sqrt((x * x) + (y * y))    # Convert cartesian to polar
-      theta = atan2(y, x)            # Convert cartesian to polar
+      r = sqrt((x * x) + (y * y))          # Convert cartesian to polar
+      theta = atan2(y, x)                  # Convert cartesian to polar
       # Compute 2D polar coordinate function
-      val = sin(n * cos(r) + 5 * theta)        # Results in a value between -1 and 1
-      #val = cos(r)                            # Another simple function
-      #val = sin(theta)                        # Another simple function
-      # Map the resulting value to a grayscale value
-      pixels[i + j * width] = color((val + 1.0) * 255.0/2.0)     # Scale to between 0 and 255
-      y += dy                # Increment y
+      val = sin(n * cos(r) + 5 * theta)    # Results in a value between -1 and 1
+      # val = cos(r)                       # Another simple function
+      # val = sin(theta)                   # Another simple function
+      # Map the resulting value to a grayscale value. Scale to between 0 and 255
+      pixels[i + j * width] = color((val + 1.0) * 255.0 / 2.0)
+      y += dy                              # Increment y
     end
-    x += dx                  # Increment x
+    x += dx                                # Increment x
   end
   update_pixels
 end
-
--- samples/processing_app/basics/math/interpolate.rb
+++ samples/processing_app/basics/math/interpolate.rb
@@ -1,33 +1,31 @@
 #
-# Linear Interpolation. 
-# 
-# Move the mouse across the screen and the symbol will follow.  
-# Between drawing each frame of the animation, the ellipse moves 
-# part of the distance (0.05) from its current position toward 
+# Linear Interpolation.
+#
+# Move the mouse across the screen and the symbol will follow.
+# Between drawing each frame of the animation, the ellipse moves
+# part of the distance (0.05) from its current position toward
 # the cursor using the lerp() function
 #
-# This is the same as the Easing under input only with lerp() instead. 
+# This is the same as the Easing under input only with lerp() instead.
 #
- 
+
 attr_reader :x, :y
 
 def setup
-  size(640, 360) 
-  no_stroke  
+  size(640, 360)
+  no_stroke
+  @x, @y = width / 2, height / 2
 end
 
-def draw 
+def draw
   background(51)
-  
-  # lerp() calculates a number between two numbers at a specific increment. 
-  # The amt parameter is the amount to interpolate between the two values 
-  # where 0.0 equal to the first point, 0.1 is very near the first point, 0.5 
-  # is half-way in between, etc.  
-  
+  # lerp() calculates a number between two numbers at a specific increment.
+  # The amt parameter is the amount to interpolate between the two values
+  # where 0.0 equal to the first point, 0.1 is very near the first point, 0.5
+  # is half-way in between, etc.
   # Here we are moving 5% of the way to the mouse location each frame
   @x = lerp(x, mouse_x, 0.05)
   @y = lerp(y, mouse_y, 0.05)
-  
   fill(255)
   stroke(255)
   ellipse(x, y, 66, 66)
